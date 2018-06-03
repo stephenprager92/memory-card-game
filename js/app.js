347c2e33f5216d13710061bb3eeb7b837a8c3d9f
Steve Prager <stephenprager92@gmail.com>
2018-06-03 18:53:58 -0400

    Prevent clicking of matched cards

--- js/app.js
+++ js/app.js
@@ -67,18 +67,6 @@ function shuffle(array) {
     return array;
 }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-
 // SET UP EVENT LISTENERS FOR CARD
 
 // First, we need our handler functions
@@ -216,23 +204,27 @@ function resetGame() {
 	for (let i = 0; i < cardList.length; i++) {
 		cardList[i].addEventListener('click', function selectCard() {
 
-			// Add active card (executed in conditional). If two active cards... 
-			if (addActiveCard(this) === activeCardMax) {
-				
-	            // Increment move counter
-				incrementMoveCounter();
-				
-				// Update the (star) score
-	            updateScore();
-		
-		        // Check for a match
-	    	    checkMatch(activeCards);
+			// Only continue if the card is not matched
+			if (!this.classList.contains('match')) {
 
-	    	    // Remove active cards from list
-				removeActiveCards();
+				// Add active card (executed in conditional). If two active cards... 
+				if (addActiveCard(this) === activeCardMax) {
+					
+		            // Increment move counter
+					incrementMoveCounter();
+					
+					// Update the (star) score
+		            updateScore();
+			
+			        // Check for a match
+		    	    checkMatch(activeCards);
 
-				// Check for a winner
-				checkWinner();
+		    	    // Remove active cards from list
+					removeActiveCards();
+
+					// Check for a winner
+					checkWinner();
+				}
 			}
 		});
 	}
@@ -256,24 +248,28 @@ const cardList = document.querySelectorAll('.card');
 for (let i = 0; i < cardList.length; i++) {
 	cardList[i].addEventListener('click', function selectCard() {
 				
-		// Add active card (executed in conditional). If two active cards... 
-		if (addActiveCard(this) === activeCardMax) {
-			
-            // Increment move counter
-			incrementMoveCounter();
-			
-			// Update the (star) score
-            updateScore();
-	
-	        // Check for a match
-    	    checkMatch(activeCards);
+		// Only continue if the card is not matched
+		if (!this.classList.contains('match')) {
 
-    	    // Remove active cards from list
-			removeActiveCards();
+			// Add active card (executed in conditional). If two active cards... 
+			if (addActiveCard(this) === activeCardMax) {
+				
+	            // Increment move counter
+				incrementMoveCounter();
+				
+				// Update the (star) score
+	            updateScore();
+		
+		        // Check for a match
+	    	    checkMatch(activeCards);
 
-			// Check for a winner
-			checkWinner();
+	    	    // Remove active cards from list
+				removeActiveCards();
 
+				// Check for a winner
+				checkWinner();
+
+			}
 		}
 	});
 }
